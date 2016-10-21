1cdaad75bc6e4061c4cf9b5bc100eb19ce523417
Shubham Lakhiwal <shubham.lakhiwal@delhivery.com>
2016-10-21 15:15:17 +0530

    initialized bower and mainjs

---
+++ main.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+    angular
+        .module('gm-directives', [])
+        .directive('focusIf', FocusIf);
+
+    FocusIf.$inject = ['$timeout'];
+
+    function FocusIf($timeout) {
+        function link($scope, $element, $attrs) {
+            var dom = $element[0];
+            if ($attrs.focusIf) {
+                $scope.$watch($attrs.focusIf, focus);
+            } else {
+                focus(true);
+            }
+            function focus(condition) {
+                if (condition) {
+                    $timeout(function() {
+                        dom.focus();
+                    }, $scope.$eval($attrs.focusDelay) || 0);
+                }
+            }
+        }
+        return {
+            restrict: 'A',
+            link: link
+        };
+    }
+})();
